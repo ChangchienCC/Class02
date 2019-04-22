20a6ecd8a3d5ac87194dd28b7884cf1cced4e7f0
ChangchienCC <changchien@mail.cjcu.edu.tw>
2019-04-22 10:05:33 +0800

    add file

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  20190415
+//
+//  Created by Changchien on 2019/4/15.
+//  Copyright © 2019 Changchien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        let a : Int = 0
+        if (a>0){
+          //  print("a>0")
+            
+        }
+        else {
+        print("a<=0")
+        }
+    
+
+    }
+}
+
